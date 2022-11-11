f60fa3afbf108432c4d29ef26dd25756075c41eb
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-11-11 16:38:21 +0200

    card layout adapted to mobile screen

--- src/components/Jobcard.tsx
+++ src/components/Jobcard.tsx
@@ -14,26 +14,28 @@ export default function Jobcard() {
                 alt="Neil "
               />
             </div>
-            <div className="min-w-0 grid gap-y-1">
-              <p className=" text-sm font-medium text-gray-900 dark:text-white">
-                Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder einen
-                Arzt/eine Ärztin (m/w/d) für die Weiterbildung zum Facharzt/ zur
-                Fachärztin für Arbeitsmedizin (m/w/d)
-              </p>
-              <p className="truncate text-sm text-gray-500 dark:text-gray-400">
-                Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
-              </p>
-
-              <div className="">
-                <p className="truncate text-sm text-gray-500 dark:text-gray-400">
-                  Vienna, Austria
+            <div className="min-w-0 flex  flex-col md:flex-row gap-y-1">
+              <div className="flex flex-col ">
+                <p className=" text-sm font-medium text-gray-900 dark:text-white">
+                  Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder
+                  einen Arzt/eine Ärztin (m/w/d) für die Weiterbildung zum
+                  Facharzt/ zur Fachärztin für Arbeitsmedizin (m/w/d)
                 </p>
+                <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                  Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
+                </p>
+                <div className="">
+                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                    Vienna, Austria
+                  </p>
+                </div>
               </div>
-            </div>
-
-            <div className="min-w-0 flex-4">here will be starts icons</div>
-            <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-              here will be bookmark and post date
+              <div className="min-w-0 flex  border md:order-last order-first">
+                <div className="min-w-0 ">here will be starts icons</div>
+                <div className="inline-flex items-center ">
+                  here will be bookmark and post date
+                </div>
+              </div>
             </div>
           </div>
         </li>
